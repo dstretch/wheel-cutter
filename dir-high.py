ec23b1745130cd9658913a629f94f25839d31c1f
dstretch <dstretch@gmail.com>
2023-12-16 17:20:04 +0000

    Update dir-high.py
    Updated dir-high.py to use gpiozero

--- dir-high.py
+++ dir-high.py
@@ -1,19 +1,13 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Device, OutputDevice # Consider using a specific device class instead?
+from gpiozero.tools import booleanized, all_values
 
 # Pin Definitons:
-enablePin = 2
-directionPin = 3
-pulsePin = 4
+direction_pin_num = 3 # Board pin 5
 
-# setup gpio
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(enablePin, GPIO.OUT)
-GPIO.setup(directionPin, GPIO.OUT)
-GPIO.setup(pulsePin, GPIO.OUT)
-
-# set direction pin HIGH
-GPIO.output(directionPin, GPIO.HIGH)
+# set up GPIO
+direction_pin = OutputDevice(direction_pin_num)
 
 print ('setting DIR- : HIGH')
+
+# disable stepper
+direction_pin.on()
